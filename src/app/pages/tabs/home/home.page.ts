8015de24d61df5ac1bafc4baf5bebc78beb82b41
Elshin Vadim <elshinvadim@gmail.com>
2025-08-31 22:39:59 +0200

    Header

--- src/app/pages/tabs/home/home.page.ts
+++ src/app/pages/tabs/home/home.page.ts
@@ -1,20 +1,50 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonToolbar, IonLabel, IonItem } from '@ionic/angular/standalone';
+import { Component, computed, inject, OnInit } from '@angular/core';
+import {
+  IonContent,
+  IonHeader,
+  IonToolbar,
+  IonItem,
+  IonLabel,
+  IonIcon,
+  IonButton,
+  IonText,
+  IonRow,
+  IonCol,
+  IonSearchbar,
+} from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import {
+  chevronDownOutline,
+  location,
+  cart,
+  notifications,
+  options,
+} from 'ionicons/icons';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonToolbar, CommonModule, FormsModule, IonLabel, IonItem]
+  imports: [
+    IonSearchbar,
+    IonCol,
+    IonRow,
+    IonText,
+    IonButton,
+    IonIcon,
+    IonLabel,
+    IonItem,
+    IonContent,
+    IonHeader,
+    IonToolbar
+],
 })
 export class HomePage implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    addIcons({ location, chevronDownOutline, cart, notifications, options });
   }
 
+  ngOnInit() {}
 }
