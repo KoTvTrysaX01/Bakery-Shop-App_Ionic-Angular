710c38ec2061b6ffaeeb5532e8f378525b2fe2af
Elshin Vadim <elshinvadim@gmail.com>
2025-09-01 18:09:36 +0200

    Special Offers & Categories & Featured Products

--- src/app/pages/tabs/home/home.page.ts
+++ src/app/pages/tabs/home/home.page.ts
@@ -1,3 +1,4 @@
+import { ListHeadingComponent } from './../../../components/list-heading/list-heading.component';
 import { Component, computed, inject, OnInit } from '@angular/core';
 import {
   IonContent,
@@ -11,7 +12,6 @@ import {
   IonRow,
   IonCol,
   IonSearchbar,
-  
 } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import {
@@ -21,8 +21,15 @@ import {
   notifications,
   options,
 } from 'ionicons/icons';
-import { ListHeadingComponent } from 'src/app/components/list-heading/list-heading.component';
-import { BannerComponent } from "src/app/components/banner/banner.component";
+import { BannerComponent } from "../../../components/banner/banner.component";
+import { ProductService } from 'src/app/services/product/product.service';
+import { CategoryService } from 'src/app/services/category/category.service';
+import { Product } from 'src/app/interfaces/product.interface';
+import { Category } from 'src/app/interfaces/category.interface';
+import { Banner } from 'src/app/interfaces/banner.interface';
+import { BannerService } from 'src/app/services/banner/banner.service';
+import { CategoriesComponent } from "src/app/components/categories/categories.component";
+import { ProductListHorizontalComponent } from "src/app/components/product-list-horizontal/product-list-horizontal.component";
 
 @Component({
   selector: 'app-home',
@@ -42,10 +49,20 @@ import { BannerComponent } from "src/app/components/banner/banner.component";
     IonHeader,
     IonToolbar,
     ListHeadingComponent,
-    BannerComponent
+    BannerComponent,
+    CategoriesComponent,
+    ProductListHorizontalComponent
 ],
 })
 export class HomePage implements OnInit {
+
+  banners = computed<Banner[]>(() => this.bannerService.getBanners());
+  categories = computed<Category[]>(() => this.categoryService.getCategories());
+  products = computed<Product[]>(() => this.productService.getProducts());
+
+  private bannerService = inject(BannerService);
+  private categoryService = inject(CategoryService);
+  private productService = inject(ProductService);
 
   constructor() {
     addIcons({ location, chevronDownOutline, cart, notifications, options });
